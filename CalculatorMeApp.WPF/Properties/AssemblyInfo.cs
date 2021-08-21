ec0d6aaf48d56a80f2999552bbb14c5e7fe0da15
MawaSoft <softmawa@gmail.com>
2021-08-22 02:23:40 +0300

    changed info

--- CalculatorMeApp.WPF/Properties/AssemblyInfo.cs
+++ CalculatorMeApp.WPF/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CalculatorMeApp.WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Calculator library for WPF applications.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AwthanSoft")]
 [assembly: AssemblyProduct("CalculatorMeApp.WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Awthan")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("21.08.22.0")]
+[assembly: AssemblyFileVersion("21.08.22.0")]
